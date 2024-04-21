10a2c4c9bb225705940e3701508104668d299256
jnrpalma <nicolas.jnr10@outlook.com>
2024-04-21 11:42:18 -0300

    feat(list): configura server para listar produto

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, inject } from '@angular/core';
 
 @Component({
   selector: 'app-list',
@@ -7,6 +8,19 @@ import { Component } from '@angular/core';
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
-export class ListComponent {
+export class ListComponent implements OnInit {
+
+
+  products: any[] = [];
+
+  httpClient = inject(HttpClient);
+
+
+  ngOnInit(): void {
+    this.httpClient.get<any>('/api/products').subscribe((products)=>{
+      this.products = products;
+    });
+  }
+
 
 }
